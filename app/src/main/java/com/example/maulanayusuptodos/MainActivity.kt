e8740fbddc352a3535fa4ba64b832ad443c11344
maulanayusup74 <maulana7437@gmail.com>
2025-10-31 13:50:04 +0700

    edit btn back

--- app/src/main/java/com/example/maulanayusuptodos/MainActivity.kt
+++ app/src/main/java/com/example/maulanayusuptodos/MainActivity.kt
@@ -19,11 +19,27 @@ class MainActivity : AppCompatActivity() {
             supportFragmentManager.findFragmentById(binding.navHostFragment.id) as NavHostFragment
         val navController = navHostFragment.navController
         binding.toolbar.setNavigationIcon(R.drawable.ic_arrow_back_custom)
-        binding.toolbar.setNavigationIconTint(getColor(R.color.white))
+        binding.toolbar.setNavigationIconTint(getColor(R.color.black))
 
         setupActionBarWithNavController(navController)
 
-        supportActionBar?.title = "Maulana Yusup TodoApp"
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            when (destination.id) {
+                R.id.todoListFragment -> {
+                    // Di halaman utama, ga ada tombol back
+                    supportActionBar?.setDisplayHomeAsUpEnabled(false)
+                    supportActionBar?.title = "Maulana Yusup TodoApp"
+                }
+                else -> {
+                    // Di halaman detail, munculkan tombol back custom
+                    supportActionBar?.setDisplayHomeAsUpEnabled(true)
+                    binding.toolbar.navigationIcon =
+                        getDrawable(R.drawable.ic_arrow_back_custom)
+                    binding.toolbar.navigationIcon?.setTint(getColor(R.color.black))
+                    supportActionBar?.title = "Todo Detail"
+                }
+            }
+        }
     }
 
     override fun onSupportNavigateUp(): Boolean {
